ac0e75f3f62d9ff9cf4e1c85578c8415be5e8298
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-10-21 10:47:00 +0800

    用两个栈实现队列

---
+++ src/TwoStack.java
@@ -0,0 +1,59 @@
+import java.util.Stack;
+/**
+ 用两个栈实现队列
+
+ 题目描述
+ 用两个栈来实现一个队列，完成队列的 Push 和 Pop 操作。
+
+ 解题思路
+ in 栈用来处理入栈（push）操作，out 栈用来处理出栈（pop）操作。一个元素进入 in 栈之后，出栈的顺序被反转。
+ 当元素要出栈时，需要先进入 out 栈，此时元素出栈顺序再一次被反转，因此出栈顺序就和最开始入栈顺序是相同的，先进入的元素先退出，这就是队列的顺序。
+ * */
+public class TwoStack {
+    Stack<Integer> in=new Stack<Integer>();
+    Stack<Integer> out=new Stack<Integer>();
+    public void push(int node){
+        in.push(node);
+    }
+    public int pop() throws Exception{
+        if(out.empty()){
+            while(!in.empty()){
+                out.push(in.pop());
+            }
+        }
+        if(out.empty()){
+            throw new Exception("queue is empty");
+        }
+        return out.pop();
+    }
+
+    public static void main(String[] args) {
+        TwoStack ts=new TwoStack();
+        ts.push(1);
+        ts.push(2);
+        try{
+            System.out.println(ts.pop());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        ts.push(3);
+        try{
+            System.out.println(ts.pop());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        try{
+            System.out.println(ts.pop());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        try{
+            System.out.println(ts.pop());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+
+
+    }
+}
