a4f6ac1bf29e1d195766b5d8ebcd3e6ae9dfaf86
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-10-21 10:02:31 +0800

    TreeLinkNode定义

---
+++ src/TreeLinkNode.java
@@ -0,0 +1,13 @@
+/**
+ TreeLinkNode∂®“Â
+ * */
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left = null;
+    TreeLinkNode right = null;
+    TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
