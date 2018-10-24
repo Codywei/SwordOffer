9229e2f37fa9aa653bb646718823852517835f73
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-10-24 16:20:52 +0800

    在 O(1) 时间内删除链表节点

---
+++ src/DeleteNode.java
@@ -0,0 +1,48 @@
+/**
+ 在 O(1) 时间内删除链表节点
+
+ 解题思路
+ ① 如果该节点不是尾节点，那么可以直接将下一个节点的值赋给该节点，然后令该节点指向下下个节点，再删除下一个节点，时间复杂度为 O(1)。
+ ② 否则，就需要先遍历链表，找到节点的前一个节点，然后让前一个节点指向 null，时间复杂度为 O(N)。
+
+
+ 综上，如果进行 N 次操作，那么大约需要操作节点的次数为 N-1+N=2N-1，其中 N-1 表示 N-1 个不是尾节点的每个节点以 O(1) 的时间复杂度操作节点的总次数，
+ N 表示 1 个尾节点以 O(N) 的时间复杂度操作节点的总次数。(2N-1)/N ~ 2，因此该算法的平均时间复杂度为 O(1)。
+ * */
+public class DeleteNode {
+    public ListNode deleteNode(ListNode head, ListNode tobeDelete) {
+        if (head == null || tobeDelete == null) {
+            return null;
+        }
+        if (tobeDelete.next != null) {
+            // 要删除的节点不是尾节点
+            ListNode next = tobeDelete.next;
+            tobeDelete.val = next.val;
+            tobeDelete.next = next.next;
+        } else {
+            ListNode cur = head;
+            while (cur.next != tobeDelete) {
+                cur = cur.next;
+            }
+            cur.next = null;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        DeleteNode dn=new DeleteNode();
+        ListNode a1=new ListNode(1);
+        ListNode a2=new ListNode(2);
+        ListNode a3=new ListNode(3);
+        ListNode a4=new ListNode(4);
+        a1.next=a2;
+        a2.next=a3;
+        a3.next=a4;
+        dn.deleteNode(a1,a3);
+        while(a1!=null){
+            System.out.println(a1.val);
+            a1=a1.next;
+        }
+
+    }
+}
