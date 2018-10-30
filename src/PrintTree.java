90c0696ad180d43deccdf60ccfd1d3f2645a2938
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-10-30 16:02:50 +0800

    二叉树分层打印

--- src/PrintTree.java
+++ src/PrintTree.java
@@ -1,9 +1,15 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 /**
- 把二叉树打印成多行
+ 1.把二叉树分层打印成多行
+ 题目描述
+ 如题
 
+ 2.按之字形顺序打印二叉树
+ 题目描述
+ 请实现一个函数按照之字形打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右至左的顺序打印，第三行按照从左到右的顺序打印，其他行以此类推。
  * */
 public class PrintTree {
     ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
@@ -22,6 +28,34 @@ public class PrintTree {
                 queue.add(node.left);
                 queue.add(node.right);
             }
+            if (list.size() != 0) {
+                ret.add(list);
+            }
+        }
+        return ret;
+    }
+
+    public ArrayList<ArrayList<Integer>> Print2(TreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> ret = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(pRoot);
+        boolean reverse = false;
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> list = new ArrayList<>();
+            int cnt = queue.size();
+            while (cnt-- > 0) {
+                TreeNode node = queue.poll();
+                if (node == null) {
+                    continue;
+                }
+                list.add(node.val);
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+            if (reverse) {
+                Collections.reverse(list);
+            }
+            reverse = !reverse;
             if (list.size() != 0) {
                 ret.add(list);
             }
@@ -52,6 +86,15 @@ public class PrintTree {
           for(int j=0;j<ret.get(i).size();j++){
               System.out.print(ret.get(i).get(j));
           }
+            System.out.println();
+
+        }
+
+        ret=printTree.Print2(t1);
+        for (int i = 0; i <ret.size(); i++) {
+            for(int j=0;j<ret.get(i).size();j++){
+                System.out.print(ret.get(i).get(j));
+            }
             System.out.println();
 
         }
