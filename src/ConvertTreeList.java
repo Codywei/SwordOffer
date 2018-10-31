042951be0a3e93821f99ae9def5525674b47dec6
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-10-31 11:19:32 +0800

    复杂链表的复制

---
+++ src/ConvertTreeList.java
@@ -0,0 +1,25 @@
+public class ConvertTreeList {
+    private TreeNode pre = null;
+    private TreeNode head = null;
+
+    public TreeNode Convert(TreeNode root) {
+        inOrder(root);
+        return head;
+    }
+
+    private void inOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        node.left = pre;
+        if (pre != null) {
+            pre.right = node;
+        }
+        pre = node;
+        if (head == null) {
+            head = node;
+        }
+        inOrder(node.right);
+    }
+}
--- src/RandomNodeClone.java
+++ src/RandomNodeClone.java
@@ -42,4 +42,25 @@ public class RandomNodeClone {
         }
         return pCloneHead;
     }
+
+    public static void main(String[] args) {
+        RandomListNode r1=new RandomListNode(1);
+        RandomListNode r2=new RandomListNode(2);
+        RandomListNode r3=new RandomListNode(3);
+        RandomListNode r4=new RandomListNode(4);
+        r1.next=r2;
+        r2.next=r3;
+        r3.next=r4;
+        r1.random=r3;
+        r2.random=r4;
+        r3.random=r1;
+        r4.random=r2;
+        RandomNodeClone rn=new RandomNodeClone();
+        RandomListNode ret=rn.Clone(r1);
+        while(ret!=null){
+            System.out.print(ret.label);
+            System.out.println(" "+ret.random.label);
+            ret=ret.next;
+        }
+    }
 }
