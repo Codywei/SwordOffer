e638ec167f9ba5b64b3aca62d041a957bf39b57a
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-10-31 10:21:17 +0800

    二叉搜索树的后序遍历序列

---
+++ src/VerifySquenceOfBST.java
@@ -0,0 +1,39 @@
+/**
+ 二叉搜索树的后序遍历序列
+
+ 题目描述
+ 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。假设输入的数组的任意两个数字都互不相同。
+ 例如，下图是后序遍历序列 1,3,2 所对应的二叉搜索树。
+ * */
+public class VerifySquenceOfBST {
+    public boolean VerifySquenceOfBST(int[] sequence) {
+        if (sequence == null || sequence.length == 0) {
+            return false;
+        }
+        return verify(sequence, 0, sequence.length - 1);
+    }
+
+    private boolean verify(int[] sequence, int first, int last) {
+        if (last - first <= 1) {
+            return true;
+        }
+        int rootVal = sequence[last];
+        int cutIndex = first;
+        while (cutIndex < last && sequence[cutIndex] <= rootVal) {
+            cutIndex++;
+        }
+        for (int i = cutIndex; i < last; i++){
+            if (sequence[i] < rootVal) {
+                return false;
+            }
+        }
+        return verify(sequence, first, cutIndex - 1) && verify(sequence, cutIndex, last - 1);
+    }
+
+    public static void main(String[] args) {
+        VerifySquenceOfBST vs=new VerifySquenceOfBST();
+        int[] sequence={1,3,2};
+        System.out.println(vs.VerifySquenceOfBST(sequence));
+
+    }
+}
