1e5ccd381f93b5185a119113544f3b442bad759c
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-10-31 11:19:07 +0800

    二叉树中和为某一值的路径

--- src/FindequalsumPath.java
+++ src/FindequalsumPath.java
@@ -31,6 +31,24 @@ public class FindequalsumPath {
     }
 
     public static void main(String[] args) {
+        TreeNode t1=new TreeNode(10);
+        TreeNode t2=new TreeNode(5);
+        TreeNode t3=new TreeNode(12);
+        TreeNode t4=new TreeNode(4);
+        TreeNode t5=new TreeNode(7);
+        t1.left=t2;
+        t1.right=t3;
+        t2.left=t4;
+        t2.right=t5;
+        FindequalsumPath fp=new FindequalsumPath();
+        ArrayList<ArrayList<Integer>> list=fp.FindPath(t1,22);
+        for (int i = 0; i<list.size(); i++) {
+            for (int j = 0; j < list.get(i).size(); j++) {
+                System.out.print(list.get(i).get(j)+" ");
+            }
+            System.out.println();
+
+        }
 
     }
 }
