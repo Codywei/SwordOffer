ec66505d1556fcb741546fda1d003fd821daa47d
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-10-31 11:23:25 +0800

    二叉搜索树与双向链表

--- src/ConvertTreeList.java
+++ src/ConvertTreeList.java
@@ -1,3 +1,9 @@
+/**
+ 二叉搜索树与双向链表
+
+ 题目描述
+ 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向。
+ * */
 public class ConvertTreeList {
     private TreeNode pre = null;
     private TreeNode head = null;
@@ -21,5 +27,21 @@ public class ConvertTreeList {
             head = node;
         }
         inOrder(node.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode t1=new TreeNode(2);
+        TreeNode t2=new TreeNode(1);
+        TreeNode t3=new TreeNode(3);
+
+
+        t1.left=t2;
+        t1.right=t3;
+        ConvertTreeList ct=new ConvertTreeList();
+        TreeNode t4=ct.Convert(t1);
+        while(t4!=null){
+            System.out.println(t4.val);
+            t4=t4.right;
+        }
     }
 }
