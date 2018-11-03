5161c07d63abfff8eafd7c59331c239938297586
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-03 11:05:14 +0800

    字符流中第一个不重复的字符

---
+++ src/FirstAppearingOnce.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ 字符流中第一个不重复的字符
+
+ 题目描述
+ 请实现一个函数用来找出字符流中第一个只出现一次的字符。例如，当从字符流中只读出前两个字符 "go" 时，第一个只出现一次的字符是 "g"。当从该字符流中读出前六个字符“google" 时，第一个只出现一次的字符是 "l"
+ **/
+public class FirstAppearingOnce {
+    private int[] cnts = new int[256];
+    private Queue<Character> queue = new LinkedList<>();
+
+    public void Insert(char ch) {
+        cnts[ch]++;
+        queue.add(ch);
+        while (!queue.isEmpty() && cnts[queue.peek()] > 1) {
+            queue.poll();
+        }
+    }
+
+    public char FirstAppearingOnce() {
+        return queue.isEmpty() ? '#' : queue.peek();
+    }
+    public static void main(String[] args) {
+        FirstAppearingOnce fa=new FirstAppearingOnce();
+        char[] nums={'a','d','d','v','d','b','c','c'};
+        for (int i = 0; i < nums.length; i++) {
+            fa.Insert(nums[i]);
+        }
+        System.out.println(fa.FirstAppearingOnce());
+    }
+}
