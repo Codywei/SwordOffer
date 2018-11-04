41a0d4ceb22a4d4ff58d8f9aaba038b97e2906c6
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-04 10:52:40 +0800

    从 1 到 n 整数中 1 出现的次数

---
+++ src/NumberOf1Between1AndN.java
@@ -0,0 +1,18 @@
+/**
+ 从 1 到 n 整数中 1 出现的次数
+ * */
+public class NumberOf1Between1AndN {
+    public int NumberOf1Between1AndN_Solution(int n) {
+        int cnt = 0;
+        for (int m = 1; m <= n; m *= 10) {
+            int a = n / m, b = n % m;
+            cnt += (a + 8) / 10 * m + (a % 10 == 1 ? b + 1 : 0);
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        NumberOf1Between1AndN ns=new NumberOf1Between1AndN();
+        System.out.println(ns.NumberOf1Between1AndN_Solution(20));
+    }
+}
