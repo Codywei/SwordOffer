ba6dcdab5328244ddae884318cce56def7defa1e
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-06 15:21:56 +0800

    把字符串转换成整数

---
+++ src/StrToInt.java
@@ -0,0 +1,41 @@
+/**
+ 把字符串转换成整数
+
+ 题目描述
+ 将一个字符串转换成一个整数，字符串不是一个合法的数值则返回 0，要求不能使用字符串转换整数的库函数。
+
+ Iuput:
+ +2147483647
+ 1a33
+ Output:
+ 2147483647
+ 0
+ * */
+public class StrToInt {
+    public int StrToInt(String str) {
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        boolean isNegative = str.charAt(0) == '-';
+        int ret = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            /* 符号判定 */
+            if (i == 0 && (c == '+' || c == '-')) {
+                continue;
+            }
+             /* 非法输入 */
+            if (c < '0' || c > '9') {
+                return 0;
+            }
+            ret = ret * 10 + (c - '0');
+        }
+        return isNegative ? -ret : ret;
+    }
+
+    public static void main(String[] args) {
+        StrToInt sti=new StrToInt();
+        String str="12324";
+        System.out.println(sti.StrToInt(str));
+    }
+}
