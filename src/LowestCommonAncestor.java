aa2a6726d1f9ede88296890dd47742afe2640fa0
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-06 15:34:57 +0800

    树中两个节点的最低公共祖先

---
+++ src/LowestCommonAncestor.java
@@ -0,0 +1,42 @@
+/**
+ 树中两个节点的最低公共祖先
+
+ 解题思路
+ 二叉查找树:二叉查找树中，两个节点 p, q 的公共祖先 root 满足 root.val >= p.val && root.val <= q.val。
+ 普通二叉树：在左右子树中查找是否存在 p 或者 q，如果 p 和 q 分别在两个子树中，那么就说明根节点就是最低公共祖先。
+ * */
+public class LowestCommonAncestor {
+
+    public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return root;
+        }
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor1(root.left, p, q);
+        }
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor1(root.right, p, q);
+        }
+        return root;
+    }
+
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor2(root.left, p, q);
+        TreeNode right = lowestCommonAncestor2(root.right, p, q);
+        return left == null ? right : right == null ? left : root;
+    }
+
+    public static void main(String[] args) {
+        LowestCommonAncestor lc=new LowestCommonAncestor();
+        TreeNode t1=new TreeNode(2);
+        TreeNode t2=new TreeNode(1);
+        TreeNode t3=new TreeNode(3);
+        t1.left=t2;
+        t1.right=t3;
+        System.out.println(lc.lowestCommonAncestor2(t1,t2,t3).val);
+    }
+
+}
