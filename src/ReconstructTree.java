9becd2d968dac4c4b223582322a126d2142c7b2d
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-08 10:27:19 +0800

    重建二叉树

--- src/ReconstructTree.java
+++ src/ReconstructTree.java
@@ -4,10 +4,7 @@ import java.util.Map;
  重建二叉树
 
  题目描述
-
-
  根据二叉树的前序遍历和中序遍历的结果，重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
-
 
  解题思路
  前序遍历的第一个值为根节点的值，使用这个值将中序遍历结果分成两部分，左部分为树的左子树中序遍历结果，右部分为树的右子树中序遍历的结果。
@@ -17,23 +14,34 @@ public class ReconstructTree {
     private Map<Integer, Integer> indexForInOrders = new HashMap<>();
 
     public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
-        for (int i = 0; i < in.length; i++) {
-            indexForInOrders.put(in[i], i);
+
+        for(int i=0;i<in.length;i++){
+            indexForInOrders.put(in[i],i);
         }
-        return reConstructBinaryTree(pre, 0, pre.length - 1, 0);
+        return reConstructBinaryTree(pre,0,pre.length-1,0);
     }
 
+    /**
+     * @param pre  前序遍历数组
+     * @param preL 前序遍历数组在当前遍历区间的最左端
+     * @param preR 前序遍历数组在当前遍历区间的最右端
+     * @param inL  中序遍历数组在当前遍历区间的最左端（用于计算当前左子树的大小）
+     * @return
+     */
     private TreeNode reConstructBinaryTree(int[] pre, int preL, int preR, int inL) {
-        if (preL > preR) {
+
+        if(preL>preR){
             return null;
         }
-        TreeNode root = new TreeNode(pre[preL]);
-        int inIndex = indexForInOrders.get(root.val);
-        int leftTreeSize = inIndex - inL;
-        root.left = reConstructBinaryTree(pre, preL + 1, preL + leftTreeSize, inL);
-        root.right = reConstructBinaryTree(pre, preL + leftTreeSize + 1, preR, inL + leftTreeSize + 1);
+        TreeNode root=new TreeNode(pre[preL]);
+        int inIndex=indexForInOrders.get(root.val);
+        int LeftTreesize=inIndex-inL;
+        root.left=reConstructBinaryTree(pre,preL+1,preL+LeftTreesize,inL);
+        root.right=reConstructBinaryTree(pre,preL+LeftTreesize+1,preR,inL+LeftTreesize+1);
         return root;
     }
+
+
 
     public void printtree(TreeNode node){
         if(node!=null) {
