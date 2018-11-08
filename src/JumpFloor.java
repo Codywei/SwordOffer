5653260f82540127132f43d903d3501256ad62a4
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-08 14:10:41 +0800

    跳台阶

--- src/JumpFloor.java
+++ src/JumpFloor.java
@@ -5,21 +5,24 @@
  一只青蛙一次可以跳上 1 级台阶，也可以跳上 2 级。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
 
  解题思路
- 减去最后的一次跳跃，可以把前1步和前2步的跳法相加
+最后的一次跳跃的结果，可以通过把前1步和前2步的跳法相加得到
  * */
 public class JumpFloor {
-    public int JumpFloor(int n) {
-        if (n <= 2) {
+
+    public int JumpFloor(int n){
+        if(n<=2){
             return n;
         }
-        int pre2 = 1, pre1 = 2;
-        int result = 1;
-        for (int i = 2; i < n; i++) {
-            result = pre2 + pre1;
-            pre2 = pre1;
-            pre1 = result;
+        int pre2=1;
+        int pre1=2;
+        int result=0;
+        while(n>2){
+            result=pre2+pre1;
+            pre2=pre1;
+            pre1=result;
+            n--;
         }
-        return result;
+          return result;
     }
 
     public static void main(String[] args) {
