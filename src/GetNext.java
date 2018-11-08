187dbd96bd71b7ce4b729a02b5773d8062d29fca
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-08 10:43:51 +0800

    二叉树的下一个结点

--- src/GetNext.java
+++ src/GetNext.java
@@ -5,8 +5,8 @@
  给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
 
  解题思路
- 如果一个节点的右子树不为空，那么该节点的下一个节点是右子树的最左节点；
- 否则，向上找第一个左链接指向的树包含该节点的祖先节点。
+ 1.如果一个节点的右子树不为空，那么该节点的下一个节点是右子树的最左节点；
+ 2.否则，向上找第一个左链接指向的树包含该节点的祖先节点。
  * */
 public class GetNext {
     public TreeLinkNode GetNext(TreeLinkNode pNode){
@@ -16,15 +16,14 @@ public class GetNext {
                 pNode=pNode.left;
             }
             return pNode;
-        }
-        else{
-           while(pNode.next!=null){
-               TreeLinkNode parent=pNode.next;
-               if(pNode==parent.left){
-                   return parent;
-               }
-               pNode=pNode.next;
-           }
+        }else{
+            while(pNode.next!=null){
+                TreeLinkNode parent=pNode.next;
+                if(parent.left==pNode){
+                    return parent;
+                }
+                pNode=pNode.next;
+            }
         }
         return null;
     }
