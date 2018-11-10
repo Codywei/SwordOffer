02ef3d56ca098848f69f428038feafd858070cfb
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-10 11:15:39 +0800

    对称的二叉树

--- src/Symmetrical.java
+++ src/Symmetrical.java
@@ -5,23 +5,26 @@
  * */
 public class Symmetrical {
     boolean isSymmetrical(TreeNode pRoot) {
-        if (pRoot == null) {
+
+        if(pRoot==null){
             return true;
         }
-        return isSymmetrical(pRoot.left, pRoot.right);
+        return isSymmetrical(pRoot.left,pRoot.right);
     }
 
     boolean isSymmetrical(TreeNode t1, TreeNode t2) {
-        if (t1 == null && t2 == null) {
+
+        if(t1==null&&t2==null){
             return true;
         }
-        if (t1 == null || t2 == null) {
+        if(t1==null||t2==null){
             return false;
         }
-        if (t1.val != t2.val) {
+        if(t1.val!=t2.val){
             return false;
         }
-        return isSymmetrical(t1.left, t2.right) && isSymmetrical(t1.right, t2.left);
+        return isSymmetrical(t1.left,t2.right)&&isSymmetrical(t1.right,t2.left);
+
     }
 
     public static void main(String[] args) {
