0adcc8d6f2a3ee4f7932b481e63ef5fe0552b33d
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-10 11:03:06 +0800

    树的子结构(子结构，我认为不是子树)

--- src/HasSubtree.java
+++ src/HasSubtree.java
@@ -1,27 +1,29 @@
 /**
-树的子结构
+树的子结构(子结构，我认为不是子树)
 
  递归
  * */
 public class HasSubtree {
     public boolean HasSubtree(TreeNode root1, TreeNode root2) {
-        if (root1 == null || root2 == null) {
+        if(root1==null){
             return false;
         }
-        return isSubtreeWithRoot(root1, root2) || HasSubtree(root1.left, root2) || HasSubtree(root1.right, root2);
+        return isSubtreeWithRoot(root1,root2)||HasSubtree(root1.left,root2)||HasSubtree(root1.right,root2);
     }
 
     private boolean isSubtreeWithRoot(TreeNode root1, TreeNode root2) {
-        if (root2 == null) {
+
+        if(root2==null){
             return true;
         }
-        if (root1 == null) {
+        if(root1==null){
             return false;
         }
-        if (root1.val != root2.val) {
+        if(root1.val!=root2.val){
             return false;
         }
-        return isSubtreeWithRoot(root1.left, root2.left) && isSubtreeWithRoot(root1.right, root2.right);
+        return isSubtreeWithRoot(root1.left,root2.left)&&isSubtreeWithRoot(root1.right,root2.right);
+
     }
 
     public static void main(String[] args) {
