d364026d5ed7c072e36aeaa67a0f325f66dd67d6
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-12 14:57:29 +0800

    礼物的最大价值

--- src/GetMost.java
+++ src/GetMost.java
@@ -11,22 +11,23 @@
  * */
 public class GetMost {
     public int getMost(int[][] values) {
-        if (values == null || values.length == 0 || values[0].length == 0) {
+        if(values==null||values.length==0||values[0].length==0){
             return 0;
         }
-        int n = values[0].length;
-        int[] dp = new int[n];
-        for (int[] value : values) {
-            dp[0] += value[0];
+        int n=values[0].length;
+        int[] dp=new int[n];
+        for(int[] value : values){
+            dp[0]+=value[0];
             for (int i = 1; i < n; i++) {
-                dp[i] = Math.max(dp[i], dp[i - 1]) + value[i];
+               dp[i]=Math.max(dp[i],dp[i-1])+value[i];
+
             }
         }
-        return dp[n - 1];
+        return dp[n-1];
     }
 
     public static void main(String[] args) {
-        int[][] nums={{1,10,3,8},{ 12,2,9,6},{ 5,7,4,11},{ 3,7,16,5}};
+        int[][] nums={{1,10,3,8},{12,2,9,6},{5,7,4,11},{3,7,16,5}};
         GetMost gm=new GetMost();
         System.out.println(gm.getMost(nums));
     }
