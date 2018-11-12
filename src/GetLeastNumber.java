a7ac161e1e37af3d5ade6f638cd906248663a32b
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-12 09:48:29 +0800

    最小的 K 个数

--- src/GetLeastNumber.java
+++ src/GetLeastNumber.java
@@ -8,7 +8,8 @@ import java.util.PriorityQueue;
  1.快速选择
  复杂度：O(N) + O(1)
  只有当允许修改数组元素时才可以使用
- 快速排序的 partition() 方法，会返回一个整数 j 使得 a[l..j-1] 小于等于 a[j]，且 a[j+1..h] 大于等于 a[j]，此时 a[j] 就是数组的第 j 大元素。可以利用这个特性找出数组的第 K 个元素，这种找第 K 个元素的算法称为快速选择算法。
+ 快速排序的 partition() 方法，会返回一个整数 j 使得 a[l..j-1] 小于等于 a[j]，且 a[j+1..h] 大于等于 a[j]，此时 a[j] 就是数组的第 j 大元素。
+ 可以利用这个特性找出数组的第 K 个元素，这种找第 K 个元素的算法称为快速选择算法。
 
  2.大小为 K 的最小堆
  复杂度：O(NlogK) + O(K)
@@ -25,7 +26,7 @@ public class GetLeastNumber {
             return ret;
         }
         findKthSmallest(nums, k - 1);
-    /* findKthSmallest 会改变数组，使得前 k 个数都是最小的 k 个数 */
+    //findKthSmallest 会改变数组，使得前 k 个数都是最小的 k 个数
         for (int i = 0; i < k; i++) {
             ret.add(nums[i]);
         }
