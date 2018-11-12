889fc30d04c4a117fbdb0b97ec5a4ccd1ccdf6b5
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-12 14:28:36 +0800

    把数字翻译成字符串

--- src/NumDecodings.java
+++ src/NumDecodings.java
@@ -4,35 +4,49 @@
  题目描述
  给定一个数字，按照如下规则翻译成字符串：0 翻译成“a”，1 翻译成“b”... 25 翻译成“z”。一个数字有多种翻译可能，
  例如 12258 一共有 5 种，分别是 bccfi，bwfi，bczi，mcfi，mzi。实现一个函数，用来计算一个数字有多少种不同的翻译方法。
+
+ 自下而上，动态规划，从最小的问题开始 ：
+ f(r)表示以r为开始（r最小取0）到最右端所组成的数字能够翻译成字符串的种数。对于长度为n的数字，f(n)=0,f(n-1)=1,求f(0)。
+ 递推公式为 f(r-2) = f(r-1)+g(r-2,r-1)*f(r)；
+ 其中，如果r-2，r-1能够翻译成字符，则g(r-2,r-1)=1，否则为0。
+ 因此，对于12258：
+ f(5) = 0
+ f(4) = 1
+ f(3) = f(4)+0 = 1
+ f(2) = f(3)+f(4) = 2
+ f(1) = f(2)+f(3) = 3
+ f(0) = f(1)+f(2) = 5
  * */
 public class NumDecodings {
-    public int numDecodings(String s) {
-        if (s == null || s.length() == 0) {
+    public  int getTranslationCount(int number){
+        if(number<0) {
             return 0;
         }
-        int n = s.length();
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = s.charAt(0) == '0' ? 0 : 1;
-        for (int i = 2; i <= n; i++) {
-            int one = Integer.valueOf(s.substring(i - 1, i));
-            if (one != 0) {
-                dp[i] += dp[i - 1];
-            }
-            if (s.charAt(i - 2) == '0') {
-                continue;
-            }
-            int two = Integer.valueOf(s.substring(i - 2, i));
-            if (two <= 26) {
-                dp[i] += dp[i - 2];
-            }
+        if(number==1) {
+            return 1;
         }
-        return dp[n];
+        return getTranslationCount(Integer.toString(number));
+    }
+    public int getTranslationCount(String number) {
+        int f1 = 0,f2 = 1,g = 0;
+        int temp;
+        for(int i=number.length()-2;i>=0;i--){
+            if(Integer.parseInt(number.charAt(i)+""+number.charAt(i+1))<26) {
+                g = 1;
+            }
+            else {
+                g = 0;
+            }
+            temp = f2;
+            f2 = f2+g*f1;
+            f1 = temp;
+        }
+        return f2;
     }
 
     public static void main(String[] args) {
         NumDecodings nd=new NumDecodings();
-        String test="12258";
-        System.out.println(nd.numDecodings(test));
+        int test=12258;
+        System.out.println(nd.getTranslationCount(test));
     }
 }
