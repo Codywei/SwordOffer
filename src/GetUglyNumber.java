1f052bc945c7efc1616371f67fb414df015a8a9d
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-12 15:36:59 +0800

    丑数

--- src/GetUglyNumber.java
+++ src/GetUglyNumber.java
@@ -6,25 +6,35 @@
  * */
 public class GetUglyNumber {
     public int GetUglyNumber_Solution(int N) {
-        if (N <= 6) {
-            return N;
+
+        if(N<=6){
+          return N;
         }
-        int i2 = 0, i3 = 0, i5 = 0;
-        int[] dp = new int[N];
-        dp[0] = 1;
-        for (int i = 1; i < N; i++) {
-            int next2 = dp[i2] * 2, next3 = dp[i3] * 3, next5 = dp[i5] * 5;
-            dp[i] = Math.min(next2, Math.min(next3, next5));
-            if (dp[i] == next2) {
+        int i2=0,i3=0,i5=0;
+        int[] dp=new int[N];
+        dp[0]=1;
+        for(int i=1;i<N;i++){
+            int next2=dp[i2]*2;
+            int next3=dp[i3]*3;
+            int next5=dp[i5]*5;
+            dp[i]=Math.min(next2,Math.min(next3,next5));
+            if(dp[i]==next2){
                 i2++;
             }
-            if (dp[i] == next3) {
+            if(dp[i]==next3){
                 i3++;
             }
-            if (dp[i] == next5) {
+            if(dp[i]==next5){
                 i5++;
             }
         }
-        return dp[N - 1];
+        return dp[N-1];
+
     }
+
+    public static void main(String[] args) {
+        GetUglyNumber gu=new GetUglyNumber();
+        System.out.println(gu.GetUglyNumber_Solution(8));
+    }
+
 }
