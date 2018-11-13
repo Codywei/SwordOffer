45735deb8609d35cca55d5d49738ae2585e9a2c0
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-13 09:55:54 +0800

    两个链表的第一个公共结点

--- src/FindFirstCommonNode.java
+++ src/FindFirstCommonNode.java
@@ -7,10 +7,12 @@
  * */
 public class FindFirstCommonNode {
     public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
-        ListNode l1 = pHead1, l2 = pHead2;
-        while (l1 != l2) {
-            l1 = (l1 == null) ? pHead2 : l1.next;
-            l2 = (l2 == null) ? pHead1 : l2.next;
+
+        ListNode l1=pHead1;
+        ListNode l2=pHead2;
+        while(l1!=l2){
+            l1=l1==null?pHead2:l1.next;
+            l2=l2==null?pHead1:l2.next;
         }
         return l1;
     }
