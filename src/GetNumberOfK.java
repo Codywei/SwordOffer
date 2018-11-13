07f7c3a6df73ea5b294d14e2c556d51ed810fd24
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-13 10:10:14 +0800

    数字在排序数组中出现的次数

--- src/GetNumberOfK.java
+++ src/GetNumberOfK.java
@@ -1,26 +1,36 @@
 /**
 数字在排序数组中出现的次数
+
+ 题目描述
+ Input:
+ nums = 1, 2, 3, 3, 3, 3, 4, 6
+ K = 3
+ Output:
+ 4
  * */
 public class GetNumberOfK {
     public int GetNumberOfK(int[] nums, int K) {
-        int first = binarySearch(nums, K);
-        int last = binarySearch(nums, K + 1);
-        return (first == nums.length || nums[first] != K) ? 0 : last - first;
+
+        int first=binarySearch(nums,K);
+        int last=binarySearch(nums,K+1);
+        return(first==nums.length||nums[first]!=K)?0:last-first;
     }
 
     private int binarySearch(int[] nums, int K) {
-        int l = 0, h = nums.length;
-        while (l < h) {
-            int m = l + (h - l) / 2;
-            if (nums[m] >= K) {
-                h = m;
+        int l=0 ;
+        int h=nums.length-1;
+        while (l<h){
+            int m=l+(h-l)/2;
+            if(nums[m]>=K){
+                h=m;
             }
-            else {
-                l = m + 1;
+            else{
+                l=m+1;
             }
         }
         return l;
     }
+
 
     public static void main(String[] args) {
         int[] nums={1,2,2,3,3,3,3,4,5};
