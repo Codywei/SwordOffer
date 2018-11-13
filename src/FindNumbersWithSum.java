aec5a787f30f4d354653989de1d8aaba92237836
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-13 10:52:37 +0800

    和为 S 的两个数字

--- src/FindNumbersWithSum.java
+++ src/FindNumbersWithSum.java
@@ -14,20 +14,22 @@ import java.util.Arrays;
  * */
 public class FindNumbersWithSum {
     public ArrayList<Integer> FindNumbersWithSum(int[] array, int sum) {
-        int i = 0, j = array.length - 1;
-        while (i < j) {
-            int cur = array[i] + array[j];
-            if (cur == sum) {
-                return new ArrayList<>(Arrays.asList(array[i], array[j]));
+
+        int i=0;
+        int j=array.length-1;
+        while(i<j){
+            int cur=array[i]+array[j];
+            if(cur==sum){
+                return new ArrayList<Integer>(Arrays.asList(array[i],array[j]));
             }
-            if (cur < sum) {
+            else if(cur<sum){
                 i++;
             }
-            else {
+            else{
                 j--;
             }
         }
-        return new ArrayList<>();
+         return new ArrayList<>();
     }
 
     public static void main(String[] args) {
