33c358be37e1a21cb8168c16f736e5a02d9ca567
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-13 11:20:41 +0800

    翻转单词顺序列

--- src/ReverseSentence.java
+++ src/ReverseSentence.java
@@ -14,23 +14,28 @@
  * */
 public class ReverseSentence {
     public String ReverseSentence(String str) {
-        int n = str.length();
-        char[] chars = str.toCharArray();
-        int i = 0, j = 0;
-        while (j <= n) {
-            if (j == n || chars[j] == ' ') {
-                reverse(chars, i, j - 1);
-                i = j + 1;
+
+        int n=str.length();
+        char[] chars=str.toCharArray();
+        int i=0;
+        int j=0;
+        while(j<=n){
+            if(j==n||chars[j]==' '){
+                reverse(chars,i,j-1);
+                i=j+1;
             }
             j++;
         }
-        reverse(chars, 0, n - 1);
-        return new String(chars);
+        reverse(chars,0,n-1);
+        return  new String(chars);
     }
 
     private void reverse(char[] c, int i, int j) {
-        while (i < j) {
-            swap(c, i++, j--);
+//        while (i < j) {
+//            swap(c, i++, j--);
+//        }
+        while(i<j){
+            swap(c,i++,j--);
         }
     }
 
