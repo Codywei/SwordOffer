edcaeff811ae36138a41dddec545ee164be10df3
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-14 09:57:38 +0800

    股票的最大利润

--- src/MaxProfit.java
+++ src/MaxProfit.java
@@ -9,16 +9,17 @@
  * */
 public class MaxProfit {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0) {
+
+        if(prices==null||prices.length==0){
             return 0;
         }
-        int soFarMin = prices[0];
-        int maxProfit = 0;
+        int soFarmin=prices[0];
+        int MaxProfit=0;
         for (int i = 1; i < prices.length; i++) {
-            soFarMin = Math.min(soFarMin, prices[i]);
-            maxProfit = Math.max(maxProfit, prices[i] - soFarMin);
+            soFarmin=Math.min(soFarmin,prices[i]);
+            MaxProfit=Math.max(MaxProfit,prices[i]-soFarmin);
         }
-        return maxProfit;
+        return MaxProfit;
     }
 
     public static void main(String[] args) {
