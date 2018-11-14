6e1dd7e01df0e09ffd59166c26c638cba2450d9e
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-14 10:02:52 +0800

    求 1+2+3+...+n

--- src/SumSolution.java
+++ src/SumSolution.java
@@ -12,8 +12,9 @@
  * */
 public class SumSolution {
     public int Sum_Solution(int n) {
-        int sum = n;
-        boolean b = (n > 0) && ((sum += Sum_Solution(n - 1)) > 0);
+
+        int sum=n;
+        boolean b=(n>0)&&((sum+=Sum_Solution(n-1))>0);
         return sum;
     }
 
