3321302ea0c32c178528ad97af2947a29df9f38f
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-14 10:57:38 +0800

    把字符串转换成整数

--- src/StrToInt.java
+++ src/StrToInt.java
@@ -13,24 +13,23 @@
  * */
 public class StrToInt {
     public int StrToInt(String str) {
-        if (str == null || str.length() == 0) {
+
+        if(str==null||str.length()==0){
             return 0;
         }
-        boolean isNegative = str.charAt(0) == '-';
-        int ret = 0;
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            /* 符号判定 */
-            if (i == 0 && (c == '+' || c == '-')) {
+        boolean isNegative=str.charAt(0)=='-';
+        int ret=0;
+        for(int i=0;i<str.length();i++){
+            char c=str.charAt(i);
+            if(i==0&&(c=='+'||c=='-')){
                 continue;
             }
-             /* 非法输入 */
-            if (c < '0' || c > '9') {
+            if(c<'0'||c>'9'){
                 return 0;
             }
-            ret = ret * 10 + (c - '0');
+            ret=ret*10+(c-'0');
         }
-        return isNegative ? -ret : ret;
+        return isNegative? -ret:ret;
     }
 
     public static void main(String[] args) {
