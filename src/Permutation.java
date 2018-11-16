25250600bbd14ef6093fa84959ad2c97936ab19b
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-11-16 10:52:29 +0800

    字符串的排列（字符串，回溯）

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 /**
- 字符串的排列
+ 字符串的排列（字符串，回溯）
 
  题目描述
  输入一个字符串，按字典序打印出该字符串中字符的所有排列。例如输入字符串 abc，则打印出由字符 a, b, c 所能排列出来的所有字符串 abc, acb, bac, bca, cab 和 cba。
