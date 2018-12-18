45a92fa15abd0535fade8d01af057b67a1fbc304
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-12-18 16:28:47 +0800

    正则表达式匹配（正则表达式）

--- src/Match.java
+++ src/Match.java
@@ -1,62 +1,65 @@
 /**
- 正则表达式匹配(不太懂)（正则表达式）
+ 正则表达式匹配（正则表达式）
 
  题目描述
  请实现一个函数用来匹配包括 '.' 和 '*' 的正则表达式。模式中的字符 '.' 表示任意一个字符，而 '*' 表示它前面的字符可以出现任意次（包含 0 次）。
 
  在本题中，匹配是指字符串的所有字符匹配整个模式。例如，字符串 "aaa" 与模式 "a.a" 和 "ab*ac*a" 匹配，但是与 "aa.a" 和 "ab*a" 均不匹配。
 
- 解题思路
- 应该注意到，'.' 是用来当做一个任意字符，而 '*' 是用来重复前面的字符。这两个的作用不同，不能把 '.' 的作用和 '*' 进行类比，从而把它当成重复前面字符一次。
+ 解题思路（来自 牛客网 披萨大叔的解答，简洁易懂，赞）
+ 当模式中的第二个字符不是“*”时：
+ 1、如果字符串第一个字符和模式中的第一个字符相匹配，那么字符串和模式都后移一个字符，然后匹配剩余的。
+ 2、如果 字符串第一个字符和模式中的第一个字符相不匹配，直接返回false。
 
- （1）如果 pattern[j] == str[i] || pattern[j] == '.', 此时dp[i][j] = dp[i-1][j-1];
- （2）如果 pattern[j] == '*'，分两种情况:
- 1: 如果pattern[j-1] != str[i] && pattern[j-1] != '.', 此时dp[i][j] = dp[i][j-2] //a*匹配0次
- 2: 如果pattern[j-1] == str[i] || pattern[j-1] == '.'
- 此时dp[i][j] = dp[i][j-2] // a*匹配0次
- 或者 dp[i][j] = dp[i][j-1] // a*匹配1次
- 或者 dp[i][j] = dp[i-1][j] // a*匹配多次
+ 而当模式中的第二个字符是“*”时：
+ 如果字符串第一个字符跟模式第一个字符不匹配，则模式后移2个字符，继续匹配。如果字符串第一个字符跟模式第一个字符匹配，可以有3种匹配方式：
+ 1、模式后移2字符，相当于x*被忽略；
+ 2、字符串后移1字符，模式后移2字符；
+ 3、字符串后移1字符，模式不变，即继续匹配字符下一位，因为*可以匹配多位
 */
 
 public class Match {
-    public boolean match(char[] str, char[] pattern) {
 
-        int m = str.length, n = pattern.length;
-        boolean[][] dp = new boolean[m + 1][n + 1];
-
-        dp[0][0] = true;
-        for (int i = 1; i <= n; i++) {
-            if (pattern[i - 1] == '*') {
-                dp[0][i] = dp[0][i - 2];
+        public boolean match(char[] str, char[] pattern) {
+            if (str == null || pattern == null) {
+                return false;
             }
+            int strIndex = 0;
+            int patternIndex = 0;
+            return matchCore(str, strIndex, pattern, patternIndex);
         }
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (str[i - 1] == pattern[j - 1] || pattern[j - 1] == '.') {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else if (pattern[j - 1] == '*') {
-                    if (pattern[j - 2] == str[i - 1] || pattern[j - 2] == '.') {
-                        // a* counts as single a
-                        dp[i][j] |= dp[i][j - 1];
-                        // a* counts as multiple a
-                        dp[i][j] |= dp[i - 1][j];
-                        // a* counts as empty
-                        dp[i][j] |= dp[i][j - 2];
-                    } else {
-                        // a* only counts as empty
-                        dp[i][j] = dp[i][j - 2];
-                    }
+        public boolean matchCore(char[] str, int strIndex, char[] pattern, int patternIndex) {
+            //有效性检验：str到尾，pattern到尾，匹配成功
+            if (strIndex == str.length && patternIndex == pattern.length) {
+                return true;
+            }
+            //pattern先到尾，匹配失败
+            if (strIndex != str.length && patternIndex == pattern.length) {
+                return false;
+            }
+            //模式第2个是*，且字符串第1个跟模式第1个匹配,分3种匹配模式；如不匹配，模式后移2位
+            if (patternIndex + 1 < pattern.length && pattern[patternIndex + 1] == '*') {
+                if ((strIndex != str.length && pattern[patternIndex] == str[strIndex]) || (pattern[patternIndex] == '.' && strIndex != str.length)) {
+                    return matchCore(str, strIndex, pattern, patternIndex + 2)//模式后移2，视为x*匹配0个字符
+                            || matchCore(str, strIndex + 1, pattern, patternIndex + 2)//视为模式匹配1个字符
+                            || matchCore(str, strIndex + 1, pattern, patternIndex);//*匹配1个，再匹配str中的下一个
+                } else {
+                    return matchCore(str, strIndex, pattern, patternIndex + 2);
                 }
             }
+            //模式第2个不是*，且字符串第1个跟模式第1个匹配，则都后移1位，否则直接返回false
+            if ((strIndex != str.length && pattern[patternIndex] == str[strIndex]) || (pattern[patternIndex] == '.' && strIndex != str.length)) {
+                return matchCore(str, strIndex + 1, pattern, patternIndex + 1);
+            }
+            return false;
         }
-        return dp[m][n];
-    }
+
 
     public static void main(String[] args) {
         Match mat=new Match();
         char []str={'a','a','a'};
-        char []pattern={'a','.','a'};
+        char []pattern={'a','.','a','*'};
         char []pattern2={'a','b','*','a','c','a'};
         System.out.println(mat.match(str,pattern));
         System.out.println(mat.match(str,pattern2));
