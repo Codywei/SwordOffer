5991192dbf4b79b7e6a23a93e020baa797e9a87b
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-12-21 11:26:14 +0800

    数字在排序数组中出现的次数（数组，二分排序）

--- src/GetNumberOfK.java
+++ src/GetNumberOfK.java
@@ -13,18 +13,19 @@ public class GetNumberOfK {
 
         int first=binarySearch(nums,K);
         int last=binarySearch(nums,K+1);
+        //fisrt越界或者nums[first]的值不是K
         return(first==nums.length||nums[first]!=K)?0:last-first;
     }
 
     private int binarySearch(int[] nums, int K) {
-        int l=0 ;
-        int h=nums.length-1;
-        while (l<h){
+        int l=0;
+        int h=nums.length;
+        while(l<h){
             int m=l+(h-l)/2;
             if(nums[m]>=K){
                 h=m;
-            }
-            else{
+            }else{
+                //边界问题一定要确定，这里nums[m]<K，说明K一定不在左半区，所以l=m+1
                 l=m+1;
             }
         }
