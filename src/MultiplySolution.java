cea470a706b2700ccae40a909f4f80b63d2a5f96
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-12-21 16:18:27 +0800

    构建乘积数组（数组）

--- src/MultiplySolution.java
+++ src/MultiplySolution.java
@@ -9,12 +9,12 @@ public class MultiplySolution {
     public int[] multiply(int[] A) {
         int n = A.length;
         int[] B = new int[n];
-         /* 从左往右累乘 */
+         /* 从左往右累乘,累乘的是索引比当前节点小的节点 */
         for (int i = 0, product = 1; i < n; product *= A[i], i++) {
             B[i] = product;
         }
 
-        /* 从右往左累乘 */
+        /* 从右往左累乘，累乘的是索引比当前节点大的节点 */
         for (int i = n - 1, product = 1; i >= 0; product *= A[i], i--) {
             B[i] *= product;
         }
