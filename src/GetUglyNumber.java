1559277abdde84f660fbca5040a9dc1c149b1f2d
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-12-21 11:06:46 +0800

    丑数（动态规划）

--- src/GetUglyNumber.java
+++ src/GetUglyNumber.java
@@ -8,15 +8,14 @@ public class GetUglyNumber {
     public int GetUglyNumber_Solution(int N) {
 
         if(N<=6){
-          return N;
+            return N;
         }
-        int i2=0,i3=0,i5=0;
         int[] dp=new int[N];
         dp[0]=1;
-        for(int i=1;i<N;i++){
-            int next2=dp[i2]*2;
-            int next3=dp[i3]*3;
-            int next5=dp[i5]*5;
+        int i2=0,i3=0,i5=0;
+        for (int i = 1; i < dp.length; i++) {
+            //每次都将2，3，5和他们还未组合过的最小元素组合，得到当前组合的最小元素。
+            int next2=dp[i2]*2,next3=dp[i3]*3,next5=dp[i5]*5;
             dp[i]=Math.min(next2,Math.min(next3,next5));
             if(dp[i]==next2){
                 i2++;
@@ -27,6 +26,7 @@ public class GetUglyNumber {
             if(dp[i]==next5){
                 i5++;
             }
+
         }
         return dp[N-1];
 
