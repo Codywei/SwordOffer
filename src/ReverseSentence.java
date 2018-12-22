053af6eba669d0d9aac7ad4389dc3ed7514fbe68
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-12-22 11:04:50 +0800

    翻转单词顺序列（字符串翻转）

--- src/ReverseSentence.java
+++ src/ReverseSentence.java
@@ -19,6 +19,7 @@ public class ReverseSentence {
         char[] chars=str.toCharArray();
         int i=0;
         int j=0;
+        //需要注意的是需要取到j=n这一位，这样方便最后一个单词的旋转
         while(j<=n){
             if(j==n||chars[j]==' '){
                 reverse(chars,i,j-1);
@@ -28,19 +29,19 @@ public class ReverseSentence {
         }
         reverse(chars,0,n-1);
         return  new String(chars);
+
     }
 
     private void reverse(char[] c, int i, int j) {
-
         while(i<j){
             swap(c,i++,j--);
         }
     }
 
     private void swap(char[] c, int i, int j) {
-        char t = c[i];
-        c[i] = c[j];
-        c[j] = t;
+        char t=c[i];
+        c[i]=c[j];
+        c[j]=t;
     }
 
     public static void main(String[] args) {
