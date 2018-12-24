2d3a9ae1e4e0559d667e3bc0e88d728a035208c7
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-12-24 14:35:57 +0800

    字符串的排列（字符串，回溯）

--- src/Permutation.java
+++ src/Permutation.java
@@ -10,12 +10,12 @@ public class Permutation {
     private ArrayList<String> ret = new ArrayList<>();
 
     public ArrayList<String> Permutation(String str) {
-        if (str.length() == 0) {
+        if(str.length()==0){
             return ret;
         }
-        char[] chars = str.toCharArray();
+        char[] chars=str.toCharArray();
         Arrays.sort(chars);
-        backtracking(chars, new boolean[chars.length], new StringBuilder());
+        backtracking(chars,new boolean[chars.length],new StringBuilder());
         return ret;
     }
 
@@ -28,7 +28,8 @@ public class Permutation {
             if (hasUsed[i]) {
                 continue;
             }
-            // 保证不重复(如“abbc”中的‘b’)
+            // 保证不重复(如“abbc”中的‘b’)，加上!hasUsed[i - 1]就是说明在i-1已经完成了一轮backtracking，使用标志位重新置为false,或是i-1位之前就有相同元素完成backtracking,导致i-1位直接跳过。
+            // 这时如果第i位和第i-1位相等，为避免重复，就不应该在同一个位置进行回溯。
             if (i != 0 && chars[i] == chars[i - 1] && !hasUsed[i - 1]) {
                 continue;
             }
@@ -38,6 +39,7 @@ public class Permutation {
             s.deleteCharAt(s.length() - 1);
             hasUsed[i] = false;
         }
+
     }
 
     public static void main(String[] args) {
